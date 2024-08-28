cc89ecf77d7ce67dc298bb7ff284e05410aa03b3
Syed Muhammad Hussain <smhussain.pk@gmail.com>
2024-08-28 22:40:12 +0500

    DLP-007 : basic project structure with add liquity base functionalty

--- tests/btbdefipool.ts
+++ tests/btbdefipool.ts
@@ -1,16 +1,17 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
-import { BtbProject } from "../target/types/btb_project";
+import { Btbdefipool } from "../target/types/btbdefipool";
 
-describe("btb-project", () => {
+describe("btbdefipool", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.BtbProject as Program<BtbProject>;
+  const program = anchor.workspace.Btbdefipool as Program<Btbdefipool>;
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const text = "First Investor";
+    const tx = await program.methods.initialize(text).rpc();
     console.log("Your transaction signature", tx);
   });
 });
